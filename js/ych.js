309e4adf43b15ae21a1f9346c75154b7034d568c
elmariefryer <fryerlem@ie.ibm.com>
2017-04-13 17:00:17 +0100

    changes after client meeting

--- js/ych.js
+++ js/ych.js
@@ -1,7 +1,13 @@
 // Javascript for Yellow Cup House: Filip Minarik (16135261)
     
 
-function hello() 
+function deliveryMessage() 
 		{
-			alert("Hello World");
+			alert("FREE OFFICE DELIVERY
+			Please register for delivery service before placing your first order
+			Minimum order of €30 for occational delivery
+			Minimum order of €10 for daily delivery
+			
+			Order before 11am for sam day delivery.
+			Delivery locally only.");
 		}
